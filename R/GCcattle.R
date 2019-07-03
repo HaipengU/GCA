ff92332496dfedc2fcba5199b501bb1db071c48a
Haipeng Yu <haipeng.u@gmail.com>
2019-07-02 21:48:48 -0400

    update

--- R/GCcattle.R
+++ R/GCcattle.R
@@ -1,9 +1,9 @@
 #' Cattle dataset 
 #'
 #' The cattle dataset is simulated with QMSim software (Sargolzaei and Schenkel, 2009).   
-#' This dataset includes 530 individuals across five generations (from founder to generation 4), 
+#' This dataset includes 2500 individuals across six generations (from founder to generation 5), 
 #' each with 10,000 single nucleotide polymorphisms spread over 29 autosomes. Single phenotype with heritability of 0.6 was simulated.
-#' Two fixed covariates of sex and unit (management unit) are available.
+#' Two fixed covariates of sex and unit are available.
 #'
 #' @docType data
 #' @name GCcattle
@@ -13,8 +13,8 @@
 #' 
 #' @format 
 #' \itemize{
-#'   \item cattle.pheno: A matrix with a dimension of 530 x 6, which includes one phenotype, two fixed covariates and pedigree information.
-#'   \item catlle.W: A 530 by 10,000 matrix, which contains marker information.
+#'   \item cattle.pheno: A matrix with a dimension of 2500 x 6, which includes one phenotype, two fixed covariates and pedigree information.
+#'   \item catlle.W: A 2500 by 10,000 matrix, which contains marker information.
 #' }
 #'
 #' @author Haipeng Yu and Gota Morota 
